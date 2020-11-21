f23ec2f6adaa42816ef70b273494a408519375dd
govind <govind.startup@gmail.com>
2020-11-21 22:23:37 +0530

    added gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='linear_algebra',
+    version='1.0',
+    packages=['numpy'],
+    url='',
+    license='Apache 2.0',
+    author='govindaraju',
+    author_email='govind.startup@gmail.com',
+    description=''
+)
